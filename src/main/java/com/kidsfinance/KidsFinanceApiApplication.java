cde578e077211672589d89d9cb526f270f8a4fd8
Adil <seyidli0604@gmail.com>
2026-09-16 22:25:22 +0400

    chore: initialize kids finance project

---
+++ src/main/java/com/kidsfinance/KidsFinanceApiApplication.java
@@ -0,0 +1,12 @@
+package com.kidsfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsFinanceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidsFinanceApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kidsfinance/backend/KidsFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kidsfinance.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsFinanceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
